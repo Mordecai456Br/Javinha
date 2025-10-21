3a82c059a114ffb1ee9abd7b4f2335d222bb62ee
Gabriel Freitas <62771225+Mordecai456Br@users.noreply.github.com>
2025-10-21 12:25:35 -0300

    first commit

---
+++ Birds/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        int myFeathers = 0;
+
+        ArrayList<String> myBirds = new ArrayList<>();
+        myBirds.add("Mordecai");
+        myBirds.add("Pica-Pau");
+        myBirds.add("Bem-Te-Vi");
+        myBirds.add("Galinha-Pintadinha");
+        myBirds.add("Galo-Carijó");
+
+        /*for (int i = 0; i < myBirds.size(); i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println(myBirds.get(i));
+        }
+        */
+        for (String birds : myBirds) {
+            IO.println(birds);
+        }
+    }
+}

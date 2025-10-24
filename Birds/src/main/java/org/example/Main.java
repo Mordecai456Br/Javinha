4983e88e39451c0e18c1d575bc66b42429d8f22a
Mordecai456Br <gabrielseixas1@gmail.com>
2025-10-24 02:03:03 -0300

    Aula

--- Birds/src/main/java/org/example/Main.java
+++ Birds/src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+Aonde quero chegar?
+User com inventário
+Caixas de ovos para nascer novos passarinhos (comprados com penas)
+Upgrades infinitos de geração de penas baseados em progressão também comprados com penas
+ */
 public class Main {
     static void main() {
 
@@ -13,20 +20,26 @@ public class Main {
         User user2 = User.createUser("Ana", "senha123");
         User user3 = User.createUser("Ana","senha123");
 
-        ArrayList<String> myBirds = new ArrayList<>();
+        /*ArrayList<String> myBirds = new ArrayList<>();
+
+        for (String birds : myBirds) {
+            System.out.println(birds);
+        }
+        */
+        Bird myBird = new Bird("Mordecai", 2);
+
+        System.out.println(myBird.pet(3, null));
+        Set<String> myBirds = new LinkedHashSet<>();
+
         myBirds.add("Mordecai");
         myBirds.add("Pica-Pau");
+        myBirds.add("Pica-Pau");
         myBirds.add("Bem-Te-Vi");
+        myBirds.add("Bem-Te-Vi");
+        myBirds.add("Galinha-Pintadinha");
         myBirds.add("Galinha-Pintadinha");
         myBirds.add("Galo-Carijó");
 
-        for (String birds : myBirds) {
-            IO.println(birds);
-        }
-
-        Bird myBird = new Bird("Mordecai", 2);
-
-        IO.println(myBird.pet(3, null));
-
+        System.out.println(myBirds);
     }
 }
